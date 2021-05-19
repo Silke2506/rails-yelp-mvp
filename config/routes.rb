4ad4ccf325769085d8308f1b18a81a23fcb79082
Silke Guthrie <silke.guthrie@gmail.com>
2021-05-19 17:02:19 +0200

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  # get '/restaurants', to: 'restaurants#index' #a visitor can see the list of all restaurants
+  # get '/restaurants/new', to: 'restaurants#new'
+
+  # post '/restaurants', to: 'restaurants#create' #a visitor can add a new restaurant and be redirected
+
+  # get '/restaurants/:id', to: 'restaurants#show' #a visitor can be redirected to the show view of that new restaurant
+
+  resources :restaurants, only: [:index, :show] do
+    resources :reviews, only: [:new, :create]
+  end
+  resources :reviews, only: [:destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,27 @@
+require 'faker'
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+Review.destroy_all
+
+
+puts "Creating restaurants..."
+
+fake_restaurant = { name: Faker::Restaurant.name, address: Faker::Address.street_address, phone_number: Faker::PhoneNumber.phone_number, category: "japanese" }
+pizza_east =  { name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", phone_number: 344, category: "italian" }
+fake_restaurant_two = { name: Faker::Restaurant.name, address: Faker::Address.street_address, phone_number: Faker::PhoneNumber.phone_number, category: "french" }
+fake_restaurant_three = { name: Faker::Restaurant.name, address: Faker::Address.street_address, phone_number: Faker::PhoneNumber.phone_number, category: "chinese" }
+fake_restaurant_four = { name: Faker::Restaurant.name, address: Faker::Address.street_address, phone_number: Faker::PhoneNumber.phone_number, category: "belgian" }
+
+
+
+[ fake_restaurant, fake_restaurant_two, fake_restaurant_three, fake_restaurant_four, pizza_east ].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
+
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
